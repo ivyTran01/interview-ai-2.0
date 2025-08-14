6a1a0eac2e63adede8d7fd2dd4f07e435b9e35f6
ivy <vynguyenaitran@gmail.com>
2025-08-13 23:40:48 -0700

    make sure no duplicate interview sessions

--- src/utils/api/interview_sessions.ts
+++ src/utils/api/interview_sessions.ts
@@ -1,28 +1,32 @@
 import {collection, query, where, getDocs, getDoc, addDoc, doc} from "firebase/firestore";
 import { db } from "@/lib/firebase/firebaseConfig";
 import {InterviewSession, InterviewSessionBase} from "@/models/interview_session";
+import { runTransaction } from "@firebase/firestore";
 
 export async function getInterviewSessionId(userId: string, interviewId: string) {
-    const q = query(
-        collection(db, "interview_sessions"),
-        where("user_id", "==", userId),
-        where("interview_id", "==", interviewId)
-    );
+    return await runTransaction(db, async (transaction) => {
+        const q = query(
+            collection(db, "interview_sessions"),
+            where("user_id", "==", userId),
+            where("interview_id", "==", interviewId)
+        );
 
-    const querySnapshot = await getDocs(q);
-    if (!querySnapshot.empty) {
-        return querySnapshot.docs[0].id;
-    }
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+            return querySnapshot.docs[0].id;
+        }
 
-    // if no session found, create a new session and return its id:
-    const sessionCollection = collection(db, "interview_sessions");
-    const newSessionBase : InterviewSessionBase = {
-        user_id: userId,
-        interview_id: interviewId,
-        created_at: new Date()
-    }
-    const newSessionDoc = await addDoc(sessionCollection, newSessionBase);
-    return newSessionDoc.id;
+        // if no session found, create a new session and return its id:
+        const sessionRef = doc(collection(db, "interview_sessions"));
+        const newSessionBase: InterviewSessionBase = {
+            user_id: userId,
+            interview_id: interviewId,
+            created_at: new Date()
+        };
+
+        transaction.set(sessionRef, newSessionBase);
+        return sessionRef.id;
+    });
 }
 
 export async function getInterviewSessionById(sessionId: string) {
